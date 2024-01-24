403af9c15d228e570d9ed04ab7955067a4d230be
littlereed <lookluwei@gmail.com>
2024-01-24 21:11:36 +0800

    feat

--- app/page.js
+++ app/page.js
@@ -1,9 +1,22 @@
+'use client';
 
+import { useRouter } from "next/navigation";
+import styles from './page.module.css';
 
 export default function Homepage() {
   
-
+  const router = useRouter()
   return (
-    <div>Homepage</div>
+    <div className={styles.page}>
+
+      <h1>Homepage</h1>
+      <button 
+        className={styles.button}
+        onClick={() => router.push('/dashboard')}
+      >
+        Enter Dashboard Page
+      </button>
+    </div>
+    
   )
 };
